099cdf0beb7efe52691666656f53ddeb88c0a28e
Jacob Somwaru <jacobsomwaru@gmail.com>
2024-03-11 00:14:06 -0400

    fix virtualenv path

--- start.sh
+++ start.sh
@@ -16,6 +16,6 @@ then
     adev runserver partyq/main.py --host 0.0.0.0 --port 80 --app-factory main
 elif [ $OSTYPE = "linux-gnu" ]
 then
-    source $VIRTUAL_ENV
+    source $VIRTUAL_ENV/bin/activate
     adev runserver partyq/main.py --host 0.0.0.0 --port 8080 --app-factory main
 fi 
