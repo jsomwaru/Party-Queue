d86c42c14f228336d58c30a3450ba9fbefb0b612
Jacob Somwaru <jacobsomwaru@gmail.com>
2024-05-12 16:28:13 -0400

    update dbus test

--- test/test_dbus.py
+++ test/test_dbus.py
@@ -1,3 +1,8 @@
 import dbus
-for service in dbus.SystemBus().list_names():
-    print(service)
+# for service in dbus.SystemBus().list_names():
+#     print(service)
+
+bus = dbus.SystemBus()
+
+proxy = bus.get_object('org.freedesktop.NetworkManager',
+                       '/org/freedesktop/NetworkManager/Devices/eth0')
