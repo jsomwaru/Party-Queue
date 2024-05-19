7fa68aa7b971bb87ab1558b9755ef2a441372cfd
Jacob Somwaru <jacobsomwaru@gmail.com>
2024-05-19 11:03:05 -0400

    update devices backend

--- partyq/bluetooth/device_backend.py
+++ partyq/bluetooth/device_backend.py
@@ -2,11 +2,15 @@ class DeviceBackend:
 
     DEFAULT_SCAN_DURATION = 10
 
-    def start_scan():
+    def start_scan(self):
         raise NotImplementedError
     
-    def stop_scan():
+    def stop_scan(self):
         raise NotImplementedError
     
-    def found_devices():
+    def found_devices(self):
         raise NotImplementedError
+    
+    def connect(self, device_id: str): 
+        raise NotImplementedError
+    
