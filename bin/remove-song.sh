60e0dc7151a91f94e6430936ca2361ca80bc0da5
Jacob Somwaru <jacobsomwaru@gmail.com>
2024-07-08 23:53:57 -0400

    fix output in remove song

--- bin/remove-song.sh
+++ bin/remove-song.sh
@@ -7,8 +7,10 @@ curl -vvv -X DELETE -H "X-AuthToken: $admin_pass"  http://$BASEURL/remove/$1
 if [ $? -gt 0 ]
 then
   echo "There was an error removing song $1 in the queue."
+  echo
   exit 1
 else
   echo "Song $1 successfully removed."
+  echo
   exit 0
 fi
