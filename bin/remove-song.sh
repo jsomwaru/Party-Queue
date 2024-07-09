7804e91529acc81aad3a51c37d7b3b17cc14d361
Jacob Somwaru <jacobsomwaru@gmail.com>
2024-07-08 23:53:35 -0400

    update remove song

--- bin/remove-song.sh
+++ bin/remove-song.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+BASEURL="${2:-localhost}"
 
-BASEURL={$2:localhost}
+admin_pass=$(cat ../media/.admin_pass)
+curl -vvv -X DELETE -H "X-AuthToken: $admin_pass"  http://$BASEURL/remove/$1 
 
-curl -X DELETE http://$BASEURL/remove/$1 
+if [ $? -gt 0 ]
+then
+  echo "There was an error removing song $1 in the queue."
+  exit 1
+else
+  echo "Song $1 successfully removed."
+  exit 0
+fi
