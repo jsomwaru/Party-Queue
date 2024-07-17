e0321313bdaa6abbe92838a2e7bd132e1476f709
Jacob Somwaru <jacobsomwaru@gmail.com>
2024-07-17 00:46:44 -0400

    update start sh

--- bin/start.sh
+++ bin/start.sh
@@ -4,21 +4,23 @@ pushd $(git rev-parse --show-toplevel)
 
 pushd components/
 id=$(docker ps --filter "name=redis" --format "{{.ID}}")
-if [ -z $id ]
-then
-    echo "Starting redis"
-    docker compose up -d
-else
-    echo "Restarting redis"
-    docker restart $id
-fi
-popd
 
 if [[ $OSTYPE =~ ^[darwin\d*] ]]
 then
+    if [ -z $id ]
+    then
+        echo "Starting redis"
+        docker compose up -d redis
+    else
+        echo "Restarting redis"
+        docker restart $id
+    fi
+    popd
     adev runserver partyq/main.py --host 0.0.0.0 --port 80 --app-factory main
 elif [ $OSTYPE = "linux-gnu" ]
 then
+    docker compose up --force-recreate -d
+    popd
     source $VIRTUAL_ENV/bin/activate
-    adev runserver partyq/main.py --host 0.0.0.0 --port 8080 --app-factory main
+    adev runserver partyq/main.py --host 127.0.0.1 --port 8080 --app-factory main
 fi 
